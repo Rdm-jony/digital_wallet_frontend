15a6d4fa3238b7af543e83acd9a1a4a57e504e80
Rdm-jony <jonydascse@gmail.com>
2025-08-21 12:39:54 +0600

    login component setup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import App from "@/App";
 import Home from "@/pages/Home";
+import Login from "@/pages/Login";
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
@@ -12,5 +13,8 @@ export const router = createBrowserRouter([
                 element: <Home />,
             },
         ],
-    },
+    },{
+        path: "/login",
+        element: <Login />,
+    }
 ]);
